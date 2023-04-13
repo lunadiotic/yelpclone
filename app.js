d7e3eb4884e564919f2e6a7def700967ceaaee73
aim <ahmadhakim404@gmail.com>
2023-04-13 21:36:33 +0700

    membuat model untuk lokasi dengan contoh datanya

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 
+// models
+const Place = require('./models/place');
+
+// connect to mongodb
+mongoose.connect('mongodb://127.0.0.1/yelp_clone')
+	.then((result) => {
+		console.log('connected to mongodb')
+	}).catch((err) => {
+		console.log(err)
+	});
+
+// view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+
 
 app.get('/', (req, res) => {
 	res.render('home');
 });
+
+app.get('/seed/places', async (req, res) => {
+	const place = new Place({
+		title: 'Empire State Building',
+		price: '$99999999',
+		description: 'A great building',
+		location: 'New York, NY'
+	})
+	await place.save()
+	res.send(place)
+})
 
 app.listen(3000, () => {
 	console.log(`server is running on http://127.0.0.1:3000`);
