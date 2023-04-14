c18ba5f636ec6a337b119fd6cad167a03c30768c
aim <ahmadhakim404@gmail.com>
2023-04-14 20:10:35 +0700

    membuat async wrapper handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const ejsMate = require('ejs-mate')
 const express = require('express');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const wrapAsync = require('./utils/wrapAsync');
 const path = require('path');
 const app = express();
 
@@ -32,39 +33,35 @@ app.get('/', (req, res) => {
 });
 
 // places routes
-app.get('/places', async (req, res) => {
+app.get('/places', wrapAsync(async (req, res) => {
 	const places = await Place.find();
 	res.render('places/index', { places });
-})
+}))
 
 app.get('/places/create', (req, res) => {
 	res.render('places/create');
 })
 
-app.post('/places', async (req, res, next) => {
-	try {
-		const place = new Place(req.body.place);
-		await place.save();
-		res.redirect('/places');
-	} catch (error) {
-		next(error)
-	}
-})
+app.post('/places', wrapAsync(async (req, res, next) => {
+	const place = new Place(req.body.place);
+	await place.save();
+	res.redirect('/places');
+}))
 
-app.get('/places/:id', async (req, res) => {
+app.get('/places/:id', wrapAsync(async (req, res) => {
 	const place = await Place.findById(req.params.id);
 	res.render('places/show', { place });
-})
+}))
 
-app.get('/places/:id/edit', async (req, res) => {
+app.get('/places/:id/edit', wrapAsync(async (req, res) => {
 	const place = await Place.findById(req.params.id);
 	res.render('places/edit', { place });
-})
+}))
 
-app.put('/places/:id', async (req, res) => {
+app.put('/places/:id', wrapAsync(async (req, res) => {
 	await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
 	res.redirect('/places');
-})
+}))
 
 app.delete('/places/:id', async (req, res) => {
 	await Place.findByIdAndDelete(req.params.id);
