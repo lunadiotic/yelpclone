734e39ce5bb11faa3be0eabe85005e33e6da2f4f
aim <ahmadhakim404@gmail.com>
2023-04-16 23:29:49 +0700

    membuat fungsi register user

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const wrapAsync = require('../utils/wrapAsync');
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
 })
 
-router.post('/register', async (req, res) => {
-    res.send(req.body)
-})
+router.post('/register', wrapAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        await User.register(user, password);
+        req.flash('success_msg', 'You are now registered and can log in');
+        res.redirect('/places');
+    } catch (error) {
+        req.flash('error_msg', error.message);
+        res.redirect('/register');
+    }
+}))
 
 module.exports = router;
