875c3241ad63cccb66e391497a03461b83d07dc7
aimeliala <idstack@outlook.co.id>
2023-04-18 12:33:59 +0700

    cara ringkas membuat route

--- routes/places.js
+++ routes/places.js
@@ -8,22 +8,17 @@ const validatePlace = require('../middlewares/validatePlace');
 const { isAuthorPlace } = require('../middlewares/isAuthor');
 const router = express.Router();
 
-
-
-
-
-router.get('/', wrapAsync(PlaceController.index))
+router.route('/')
+    .get(wrapAsync(PlaceController.index))
+    .post(isAuth, validatePlace, wrapAsync(PlaceController.store))
 
 router.get('/create', isAuth, PlaceController.create)
 
-router.post('/', isAuth, validatePlace, wrapAsync(PlaceController.store))
-
-router.get('/:id', isValidObjectId('/places'), wrapAsync(PlaceController.show))
+router.route('/:id')
+    .get(isValidObjectId('/places'), wrapAsync(PlaceController.show))
+    .put(isAuth, isAuthorPlace, isValidObjectId('/places'), validatePlace, wrapAsync(PlaceController.update))
+    .delete(isAuth, isAuthorPlace, isValidObjectId('/places'), wrapAsync(PlaceController.destroy))
 
 router.get('/:id/edit', isAuth, isAuthorPlace, isValidObjectId('/places'), wrapAsync(PlaceController.edit))
-
-router.put('/:id', isAuth, isAuthorPlace, isValidObjectId('/places'), validatePlace, wrapAsync(PlaceController.update))
-
-router.delete('/:id', isAuth, isAuthorPlace, isValidObjectId('/places'), wrapAsync(PlaceController.destroy))
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,21 +5,21 @@ const AuthController = require('../controllers/auth')
 const wrapAsync = require('../utils/wrapAsync');
 const passport = require('passport');
 
-router.get('/register', (req, res) => {
-    res.render('auth/register');
-})
+router.route('/register')
+    .get((req, res) => {
+        res.render('auth/register');
+    })
+    .post(wrapAsync(AuthController.register))
 
-router.post('/register', wrapAsync(AuthController.register))
-
-router.get('/login', AuthController.loginForm)
-
-router.post('/login', passport.authenticate('local', {
-    failureRedirect: '/login',
-    failureFlash: {
-        type: 'error_msg',
-        msg: 'Invalid username or password'
-    }
-}), AuthController.login)
+router.route('/login')
+    .get(AuthController.loginForm)
+    .post(passport.authenticate('local', {
+        failureRedirect: '/login',
+        failureFlash: {
+            type: 'error_msg',
+            msg: 'Invalid username or password'
+        }
+    }), AuthController.login)
 
 router.post('/logout', AuthController.logout)
 
